fa23643616c0c901217ddbb2de11d576072cfb3c
Christopher Waldon <christopher.waldon.dev@gmail.com>
2017-05-09 09:28:33 -0400

    Update test to be runnable on Travis
    The previous version of this test used a temporary file, which
    TravisCI doesn't allow. This version uses a pipe to hold the
    contents of stdout instead, and should therefore work on Travis.
    
    That being said, we don't even need this test. This was just a
    demo of capturing stdout in Go because I didn't know how to do it.
    Once our code actually does something, we will delete this and
    only look back if we need to do this at some point in the future.

--- main_test.go
+++ main_test.go
@@ -4,26 +4,32 @@ import (
 	"bytes"
 	"io"
 	"os"
-	"path"
 	"strings"
 	"testing"
 )
 
 func TestMain(t *testing.T) {
 	stdout := os.Stdout
-	tmpStdout, err := os.Create(path.Join(os.TempDir(), "test-stdout"))
+	// Credit to Evan Shaw for this way to capture stdout without creating
+	// a new file. See:
+	// http://stackoverflow.com/a/10476304/7106084
+	readEnd, writeEnd, err := os.Pipe()
 	if err != nil {
-		t.Skip("Unable to create temporary test file")
+		t.Skip("Unable to create pipe to replace stdout")
 	}
-	os.Stdout = tmpStdout
-	main()
+	os.Stdout = writeEnd
+	// Send anything written on stdout back on the contents channel as a string
+	contents := make(chan string)
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, readEnd)
+		contents <- buf.String()
+	}()
+	main() //print to stdout
 	os.Stdout = stdout
-	contents := make([]byte, 256)
-	_, err = io.ReadFull(tmpStdout, contents)
-	if err != nil {
-		t.Skip("Unable to read output buffer")
-	}
-	stringContents := bytes.NewBuffer(contents).String()
+	writeEnd.Close() // tell our goroutine that we're done writing
+
+	stringContents := <-contents // wait for goroutine to give us output
 	if !strings.Contains(stringContents, "matterleast") {
 		t.Error("Stdout incorrect; expected the word 'matterleast'")
 	}
