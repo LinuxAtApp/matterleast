e2804b1c47fb45dfd3944d149693968bb8e68444
Jesse Cochran <jcochran800@gmail.com>
2017-05-17 21:04:59 -0400

    Update notify.go

--- spikes/notify/notify.go
+++ spikes/notify/notify.go
@@ -9,14 +9,14 @@ import (
 func Display(header string, body string, isUrgent bool, iconPath string) {
 	switch runtime.GOOS {
 	case "darwin":
-		//Display dialog on MacOS
+		//Display notification on MacOS
         exec.Command("osascript", "-e", "display notification \""+body+"\" with title \""+header+"\"").Run()
 	case "linux":
 		if isUrgent {
-			//Display on Linux if urgent(will open until manually closed)
+			//Display notification on Linux if urgent(will open until manually closed)
 			exec.Command("notify-send", "-i", iconPath, header, body, "-u", "critical").Run()
 		} else {
-			//Display on Linux if not urgent
+			//Display notification on Linux if not urgent
 			exec.Command("notify-send", "-i", iconPath, header, body).Run()
 		}
 	}
