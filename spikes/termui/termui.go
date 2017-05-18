b456f292eef9f8434148c6af498118e74bb85ae0
camwood23 <camwood23@gmail.com>
2017-05-18 14:00:24 -0400

    Added some basic functionality to textbox

--- spikes/termui/termui.go
+++ spikes/termui/termui.go
@@ -4,10 +4,12 @@ import "github.com/nsf/termbox-go"
 
 type TextBox struct {
 	text string
+	cursor_pos int
 }
 
 type ChatHistory struct {
 	history []string
+	authors []string
 }
 
 var text_box = TextBox{}
@@ -30,7 +32,54 @@ func redraw_all() {
 	for i := l; i >= 0; i-- {
 		tbprint(0, y-2-(l-i), termbox.ColorWhite, termbox.ColorDefault, "You: "+chat.history[i])
 	}
+	termbox.SetCursor(15+text_box.cursor_pos,y-1)
 	termbox.Flush()
+}
+
+func handle_backspace() {
+	if len(text_box.text) > 0 && text_box.cursor_pos > 0 {
+		text := text_box.text[:text_box.cursor_pos-1]
+		if text_box.cursor_pos != len(text_box.text) {
+			text += text_box.text[text_box.cursor_pos:]
+		}
+		text_box.text = text
+		text_box.cursor_pos--
+	}
+}
+
+func handle_textinput(ch rune) {
+	switch {
+		case len(text_box.text) == text_box.cursor_pos:
+			text_box.text += string(ch)
+		case text_box.cursor_pos == 0:
+			text_box.text = string(ch) + text_box.text
+		default:
+			text_box.text = text_box.text[:text_box.cursor_pos] + string(ch) + text_box.text[text_box.cursor_pos:]
+	}
+	text_box.cursor_pos++
+}
+
+func handle_cursorleft() {
+	if text_box.cursor_pos > 0 {
+		text_box.cursor_pos--
+	}
+}
+
+func handle_cursorright() {
+	if text_box.cursor_pos < len(text_box.text) {
+		text_box.cursor_pos++
+	}
+}
+
+func handle_enter() {
+	// Can also send message to back end here
+	chat.history = append(chat.history, text_box.text)
+	text_box.text = ""
+	text_box.cursor_pos = 0
+}
+
+// TODO: This function will receive messages to be added to the history
+func Receive_Message(message string) {
 }
 
 func main() {
@@ -50,10 +99,18 @@ mainloop:
 					case termbox.KeyEsc:
 						break mainloop
 					case termbox.KeyEnter:
-						chat.history = append(chat.history, text_box.text)
-						text_box.text = ""
+						handle_enter()
+					case termbox.KeyBackspace, termbox.KeyBackspace2:
+						handle_backspace()
+					case termbox.KeyArrowLeft:
+						handle_cursorleft()
+					case termbox.KeyArrowRight:
+						handle_cursorright()
+					case termbox.KeyArrowUp:
+					case termbox.KeyArrowDown:
+					//TODO: Find better way to handle this
 					default:
-						text_box.text += string(ev.Ch)
+						handle_textinput(ev.Ch)
 				}
 			case termbox.EventError:
 				panic(ev.Err)
