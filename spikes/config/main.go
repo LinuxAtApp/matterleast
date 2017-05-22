dbb831d69b873e01c000b38e03661d438f95bdf5
Christopher Waldon <christopher.waldon.dev@gmail.com>
2017-05-21 23:33:48 -0400

    Open the config file

--- spikes/config/main.go
+++ spikes/config/main.go
@@ -6,6 +6,18 @@ import (
 	"path"
 )
 
+const BUFFER_SIZE = 1024
+
+// fatal crashes the program if the given error is non-nil
+// This isn't a good way to perform production error-handling,
+// but it will serve for this demo.
+func fatal(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	xdgConfigPath := os.Getenv("XDG_CONFIG_HOME")
 	if xdgConfigPath == "" {
@@ -17,4 +29,12 @@ func main() {
 		xdgConfigPath = path.Join(home, ".config")
 	}
 	fmt.Println("Config Dir:", xdgConfigPath)
+	configFile, err := os.Open(path.Join(xdgConfigPath, "matterleast.conf"))
+	fatal(err)
+
+	defer configFile.Close()
+	data := make([]byte, BUFFER_SIZE)
+	bytesRead, err := configFile.Read(data)
+	fatal(err)
+
 }
