efbb3c14cdd42362498be6aa2d1b53a42f287335
Andrew Thorp <ahthorp96@icloud.com>
2017-05-25 12:55:25 -0400

    Fixed travis issue

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/LinuxAtApp/matterleast/servercom"
 )
-
+/*
+Main usage: `go run main.go -u <username> -p <password> <url>
+Package demontrates simple login functions using the servercom package's methods.
+*/
 func main() {
-	fmt.Println("matterleast")
+	username := flag.String("u", "", "Username")
+	password := flag.String("p", "", "Password")
+	flag.Parse()
+	url := flag.Arg(0)
+	//Creates client and logs user in.
+	client:= *servercom.Startup(url, *username, *password)
+	//Tests if login was successful. 
+	if servercom.Connected(client) {
+		fmt.Println("*Connection Successful!*")
+	} else {
+		fmt.Println("*You are not connected.*")
+	}
 }
