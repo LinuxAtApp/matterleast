c4dbbc4d59d9b94c1e5b39ab2a037a20288e5e32
Christopher Waldon <christopher.waldon.dev@gmail.com>
2017-07-02 17:07:57 -0400

    Create factory functions for client interfaces

--- servercom/servercom.go
+++ servercom/servercom.go
@@ -4,35 +4,49 @@ import (
 	mm "github.com/mattermost/platform/model"
 )
 
-type Client interface {
-    Login(loginId string, password string) (*mm.Result, *mm.AppError)
-    GetTeamByName(teamName string) (*mm.Result, *mm.AppError)
-    SetTeamId(teamId string)
-    GetAllTeamListings() (*mm.Result, *mm.AppError)
-    GetChannelByName(channelName string) (*mm.Result, *mm.AppError)
-    GetChannels(etag string) (*mm.Result, *mm.AppError)
-    CreatePost(post *mm.Post) (*mm.Result, *mm.AppError)
-    GetPostsSince(channelId string, time int64) (*mm.Result, *mm.AppError)
-}
+type (
+	Client interface {
+		Login(loginId string, password string) (*mm.Result, *mm.AppError)
+		GetTeamByName(teamName string) (*mm.Result, *mm.AppError)
+		SetTeamId(teamId string)
+		GetAllTeamListings() (*mm.Result, *mm.AppError)
+		GetChannelByName(channelName string) (*mm.Result, *mm.AppError)
+		GetChannels(etag string) (*mm.Result, *mm.AppError)
+		CreatePost(post *mm.Post) (*mm.Result, *mm.AppError)
+		GetPostsSince(channelId string, time int64) (*mm.Result, *mm.AppError)
+	}
 
-// WSClient is a websocket client to the mattermost server
-type WSClient interface {
-    Close()
-    Listen()
-    Events() chan *mm.WebSocketEvent
-}
+	// WSClient is a websocket client to the mattermost server
+	WSClient interface {
+		Close()
+		Listen()
+		Events() chan *mm.WebSocketEvent
+	}
 
-// wsWrapper wraps an existing websocket client implementation so that the event
-// channel can be accessed via a method (rather than as a field). This allow us
-// to hide the WSClient completely behind an interface and swap the implementation
-// in the future if need be.
-type wsWrapper struct {
-    *mm.WebSocketClient
-}
+	// wsWrapper wraps an existing websocket client implementation so that the event
+	// channel can be accessed via a method (rather than as a field). This allow us
+	// to hide the WSClient completely behind an interface and swap the implementation
+	// in the future if need be.
+	wsWrapper struct {
+		*mm.WebSocketClient
+	}
+
+	// ServerCom acts as a mitigator between the frontend and the mattermost model API.
+	ServerCom struct {
+		Client    Client
+		Team      mm.Team
+		Channel   *mm.Channel
+		Events    chan *mm.WebSocketEvent
+		Responses chan *mm.WebSocketResponse
+	}
+
+	ClientFactory   func(url string) Client
+	WSClientFactory func(url, authToken string) (WSClient, error)
+)
 
 // Events returns the stream of realtime events from the mattermost server.
 func (w *wsWrapper) Events() chan *mm.WebSocketEvent {
-    return w.WebSocketClient.EventChannel
+	return w.WebSocketClient.EventChannel
 }
 
 // test that wsWrapper implements WSClient at compile-time
@@ -41,22 +55,22 @@ var _ WSClient = &wsWrapper{}
 // test that *mm.Client satisfies the Client interface at compile time
 var _ Client = &mm.Client{}
 
-/*
-ServerCom acts as a mitigator between the frontend and the mattermost model API.
-*/
-type ServerCom struct {
-	Client  Client
-	Team    mm.Team
-	Channel *mm.Channel
-	Events  chan *mm.WebSocketEvent
-	Responses chan *mm.WebSocketResponse
+var NewClient ClientFactory = func(url string) Client {
+	return mm.NewClient(url)
+}
+var NewWSClient WSClientFactory = func(url, authToken string) (WSClient, error) {
+	wsc, err := mm.NewWebSocketClient(url, authToken)
+	if err != nil {
+		return nil, err
+	}
+	return &wsWrapper{wsc}, nil
 }
 
 /*
 Startup accepts the url and login credentials for a user, and returns a new serverCom struct.
 */
 func Startup(url string, username string, password string) (*ServerCom, error) {
-	ServerCom := &ServerCom{Client: mm.NewClient(url)}
+	ServerCom := &ServerCom{Client: NewClient(url)}
 	_, err := ServerCom.Client.Login(username, password)
 	if err != nil {
 		return nil, err
